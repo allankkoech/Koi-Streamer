3fd8913f9626bdfe25d95ed921f78287e4efb747
Allan Koech <allankoech.ke@gmail.com>
2023-01-13 13:24:57 +0300

    Added theme functionality

--- src/backend/apptheme.cpp
+++ src/backend/apptheme.cpp
@@ -5,3 +5,255 @@ AppTheme::AppTheme(QObject *parent)
 {
 
 }
+
+void AppTheme::setWindowDimensions(float width, float height)
+{
+    // Window in landscape mode if width > height
+    // else portrait
+    setIsLandscapeMode(width > height);
+
+    // Update dimensions
+    setScreenWidth(width);
+    setScreenHeight(height);
+}
+
+void AppTheme::selectThemePallette(const QString &palletteName)
+{
+    if(m_pallettes.size() == 0)
+        return;
+
+    auto pallette = m_pallettes[palletteName];
+
+    setThemeName(palletteName);
+    setBackgroundColor(pallette["background"].toString());
+    setForegroundColor(pallette["foreground"].toString());
+    setTextColor(pallette["text"].toString());
+    setSelectionBackgroud(pallette["selectionBackground"].toString());
+    setSelectionForeground(pallette["selectionForeground"].toString());
+    setButtonColor(pallette["buttons"].toString());
+    setDisabledColor(pallette["disabled"].toString());
+    setActiveColor(pallette["active"].toString());
+    setBorderColor(pallette["border"].toString());
+    setHighlightColor(pallette["highlight"].toString());
+    setAccentColor(pallette["accent"].toString());
+}
+
+const QString &AppTheme::themeName() const
+{
+    return m_themeName;
+}
+
+void AppTheme::setThemeName(const QString &newThemeName)
+{
+    if (m_themeName == newThemeName)
+        return;
+    m_themeName = newThemeName;
+    emit themeNameChanged();
+}
+
+const QString &AppTheme::backgroundColor() const
+{
+    return m_backgroundColor;
+}
+
+void AppTheme::setBackgroundColor(const QString &newBackgroundColor)
+{
+    if (m_backgroundColor == newBackgroundColor)
+        return;
+    m_backgroundColor = newBackgroundColor;
+    emit backgroundColorChanged();
+}
+
+const QString &AppTheme::foregroundColor() const
+{
+    return m_foregroundColor;
+}
+
+void AppTheme::setForegroundColor(const QString &newForegroundColor)
+{
+    if (m_foregroundColor == newForegroundColor)
+        return;
+    m_foregroundColor = newForegroundColor;
+    emit foregroundColorChanged();
+}
+
+const QString &AppTheme::textColor() const
+{
+    return m_textColor;
+}
+
+void AppTheme::setTextColor(const QString &newTextColor)
+{
+    if (m_textColor == newTextColor)
+        return;
+    m_textColor = newTextColor;
+    emit textColorChanged();
+}
+
+const QString &AppTheme::selectionBackgroud() const
+{
+    return m_selectionBackgroud;
+}
+
+void AppTheme::setSelectionBackgroud(const QString &newSelectionBackgroud)
+{
+    if (m_selectionBackgroud == newSelectionBackgroud)
+        return;
+    m_selectionBackgroud = newSelectionBackgroud;
+    emit selectionBackgroudChanged();
+}
+
+const QString &AppTheme::selectionForeground() const
+{
+    return m_selectionForeground;
+}
+
+void AppTheme::setSelectionForeground(const QString &newSelectionForeground)
+{
+    if (m_selectionForeground == newSelectionForeground)
+        return;
+    m_selectionForeground = newSelectionForeground;
+    emit selectionForegroundChanged();
+}
+
+const QString &AppTheme::buttonColor() const
+{
+    return m_buttonColor;
+}
+
+void AppTheme::setButtonColor(const QString &newButtonColor)
+{
+    if (m_buttonColor == newButtonColor)
+        return;
+    m_buttonColor = newButtonColor;
+    emit buttonColorChanged();
+}
+
+const QString &AppTheme::disabledColor() const
+{
+    return m_disabledColor;
+}
+
+void AppTheme::setDisabledColor(const QString &newDisabledColor)
+{
+    if (m_disabledColor == newDisabledColor)
+        return;
+    m_disabledColor = newDisabledColor;
+    emit disabledColorChanged();
+}
+
+const QString &AppTheme::activeColor() const
+{
+    return m_activeColor;
+}
+
+void AppTheme::setActiveColor(const QString &newActiveColor)
+{
+    if (m_activeColor == newActiveColor)
+        return;
+    m_activeColor = newActiveColor;
+    emit activeColorChanged();
+}
+
+const QString &AppTheme::borderColor() const
+{
+    return m_borderColor;
+}
+
+void AppTheme::setBorderColor(const QString &newBorderColor)
+{
+    if (m_borderColor == newBorderColor)
+        return;
+    m_borderColor = newBorderColor;
+    emit borderColorChanged();
+}
+
+const QString &AppTheme::highlightColor() const
+{
+    return m_highlightColor;
+}
+
+void AppTheme::setHighlightColor(const QString &newHighlightColor)
+{
+    if (m_highlightColor == newHighlightColor)
+        return;
+    m_highlightColor = newHighlightColor;
+    emit highlightColorChanged();
+}
+
+const QString &AppTheme::accentColor() const
+{
+    return m_accentColor;
+}
+
+void AppTheme::setAccentColor(const QString &newAccentColor)
+{
+    if (m_accentColor == newAccentColor)
+        return;
+    m_accentColor = newAccentColor;
+    emit accentColorChanged();
+}
+
+bool AppTheme::isLandscapeMode() const
+{
+    return m_isLandscapeMode;
+}
+
+void AppTheme::setIsLandscapeMode(bool newIsLandscapeMode)
+{
+    if (m_isLandscapeMode == newIsLandscapeMode)
+        return;
+    m_isLandscapeMode = newIsLandscapeMode;
+    emit isLandscapeModeChanged();
+}
+
+float AppTheme::screenWidth() const
+{
+    return m_screenWidth;
+}
+
+void AppTheme::setScreenWidth(float newScreenWidth)
+{
+    if (qFuzzyCompare(m_screenWidth, newScreenWidth))
+        return;
+    m_screenWidth = newScreenWidth;
+    emit screenWidthChanged();
+}
+
+float AppTheme::screenHeight() const
+{
+    return m_screenHeight;
+}
+
+void AppTheme::setScreenHeight(float newScreenHeight)
+{
+    if (qFuzzyCompare(m_screenHeight, newScreenHeight))
+        return;
+    m_screenHeight = newScreenHeight;
+    emit screenHeightChanged();
+}
+
+void AppTheme::loadThemes()
+{
+    QFile themeFile;
+    themeFile.setFileName(":/src/frontend/Theme.json");
+    themeFile.open(QIODevice::ReadOnly);
+    auto data = themeFile.readAll();
+
+    // Create JSON Object
+    QJsonDocument themeDoc = QJsonDocument::fromJson(data.data());
+    QJsonObject themeObj = themeDoc.object();
+    QJsonArray pallette = themeObj["pallette"].toArray();
+
+    m_pallettes.clear();
+
+    for(const auto &pal : pallette) {
+        QJsonObject palObj = pal.toObject();
+        QString name = palObj["name"].toString();
+        QJsonObject style = palObj["style"].toObject();
+
+        m_pallettes[name] = style;
+    }
+
+    selectThemePallette(pallette[0].toObject()["name"].toString());
+}
--- src/backend/apptheme.h
+++ src/backend/apptheme.h
@@ -2,6 +2,12 @@
 #define APPTHEME_H
 
 #include <QObject>
+#include <QDebug>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QFile>
+#include <QJsonArray>
+
 
 class AppTheme : public QObject
 {
@@ -9,8 +15,133 @@ class AppTheme : public QObject
 public:
     explicit AppTheme(QObject *parent = nullptr);
 
+    // Color Pallettes
+    Q_PROPERTY(QString themeName READ themeName WRITE setThemeName NOTIFY themeNameChanged)
+    Q_PROPERTY(QString backgroundColor READ backgroundColor WRITE setBackgroundColor NOTIFY backgroundColorChanged)
+    Q_PROPERTY(QString foregroundColor READ foregroundColor WRITE setForegroundColor NOTIFY foregroundColorChanged)
+    Q_PROPERTY(QString textColor READ textColor WRITE setTextColor NOTIFY textColorChanged)
+    Q_PROPERTY(QString selectionBackgroud READ selectionBackgroud WRITE setSelectionBackgroud NOTIFY selectionBackgroudChanged)
+    Q_PROPERTY(QString selectionForeground READ selectionForeground WRITE setSelectionForeground NOTIFY selectionForegroundChanged)
+    Q_PROPERTY(QString buttonColor READ buttonColor WRITE setButtonColor NOTIFY buttonColorChanged)
+    Q_PROPERTY(QString disabledColor READ disabledColor WRITE setDisabledColor NOTIFY disabledColorChanged)
+    Q_PROPERTY(QString activeColor READ activeColor WRITE setActiveColor NOTIFY activeColorChanged)
+    Q_PROPERTY(QString borderColor READ borderColor WRITE setBorderColor NOTIFY borderColorChanged)
+    Q_PROPERTY(QString highlightColor READ highlightColor WRITE setHighlightColor NOTIFY highlightColorChanged)
+    Q_PROPERTY(QString accentColor READ accentColor WRITE setAccentColor NOTIFY accentColorChanged)
+
+    // Dimensions
+    Q_PROPERTY(bool isLandscapeMode READ isLandscapeMode WRITE setIsLandscapeMode NOTIFY isLandscapeModeChanged)
+    Q_PROPERTY(float screenWidth READ screenWidth WRITE setScreenWidth NOTIFY screenWidthChanged)
+    Q_PROPERTY(float screenHeight READ screenHeight WRITE setScreenHeight NOTIFY screenHeightChanged)
+
+    // Invokables
+    /**
+     * @brief setWindowDimensions
+     * @param width Current width of the screen
+     * @param height Current height of the screen
+     */
+    Q_INVOKABLE void setWindowDimensions(float width, float height);
+
+    Q_INVOKABLE void selectThemePallette(const QString& palletteName);
+
+
+    const QString &themeName() const;
+    void setThemeName(const QString &newThemeName);
+
+    const QString &backgroundColor() const;
+    void setBackgroundColor(const QString &newBackgroundColor);
+
+    const QString &foregroundColor() const;
+    void setForegroundColor(const QString &newForegroundColor);
+
+    const QString &textColor() const;
+    void setTextColor(const QString &newTextColor);
+
+    const QString &selectionBackgroud() const;
+    void setSelectionBackgroud(const QString &newSelectionBackgroud);
+
+    const QString &selectionForeground() const;
+    void setSelectionForeground(const QString &newSelectionForeground);
+
+    const QString &buttonColor() const;
+    void setButtonColor(const QString &newButtonColor);
+
+    const QString &disabledColor() const;
+    void setDisabledColor(const QString &newDisabledColor);
+
+    const QString &activeColor() const;
+    void setActiveColor(const QString &newActiveColor);
+
+    const QString &borderColor() const;
+    void setBorderColor(const QString &newBorderColor);
+
+    const QString &highlightColor() const;
+    void setHighlightColor(const QString &newHighlightColor);
+
+    const QString &accentColor() const;
+    void setAccentColor(const QString &newAccentColor);
+
+    bool isLandscapeMode() const;
+    void setIsLandscapeMode(bool newIsLandscapeMode);
+
+    float screenWidth() const;
+    void setScreenWidth(float newScreenWidth);
+
+    float screenHeight() const;
+    void setScreenHeight(float newScreenHeight);
+
 signals:
 
+    void themeNameChanged();
+
+    void backgroundColorChanged();
+
+    void foregroundColorChanged();
+
+    void textColorChanged();
+
+    void selectionBackgroudChanged();
+
+    void selectionForegroundChanged();
+
+    void buttonColorChanged();
+
+    void disabledColorChanged();
+
+    void activeColorChanged();
+
+    void borderColorChanged();
+
+    void highlightColorChanged();
+
+    void accentColorChanged();
+
+    void isLandscapeModeChanged();
+
+    void screenWidthChanged();
+
+    void screenHeightChanged();
+
+private:
+    void loadThemes();
+
+    QString m_themeName;
+    QString m_backgroundColor;
+    QString m_foregroundColor;
+    QString m_textColor;
+    QString m_selectionBackgroud;
+    QString m_selectionForeground;
+    QString m_buttonColor;
+    QString m_disabledColor;
+    QString m_activeColor;
+    QString m_borderColor;
+    QString m_highlightColor;
+    QString m_accentColor;
+    bool m_isLandscapeMode;
+    float m_screenWidth;
+    float m_screenHeight;
+
+    QMap<QString, QJsonObject> m_pallettes;
 };
 
 #endif // APPTHEME_H
